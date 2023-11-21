fb1ebfcf271d643e84fd4a0638e67e1e21e1c089
sahid264 <149563136+sahid264@users.noreply.github.com>
2023-11-21 00:00:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function buttonclick(val)
+{
+    document.getElementById("screen").value=document.getElementById("screen").value+val
+}
+function cleardisplay(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
+
+function deletedisplay(){
+    var display = document.getElementById("screen")
+    display.value = display.value.slice(0, -1)
+}
